2efed9ec4e1a85ea1ad1a71f9aa2f078947739a5
tabit <tabithakavyu4@gmail.com>
2020-10-28 12:56:41 +0300

    Props

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,27 +1,42 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-
-
-//CSS
 import './index.css'
+
+const firstBook = {
+    titlem :'Becoming: A Guided Journal for Discovering Your Voice',
+    author : 'Michelle Obama',
+    img : "https://m.media-amazon.com/images/I/81bFNmhKrTL._AC_UY218_.jpg"
+
+}
+
+const secondBook = {
+    title:'Conversations with RBG: Ruth Bader Ginsburg on Life, Love, Liberty, and Law',
+    author : 'Jeffery Rosen',
+    img : "https://images-na.ssl-images-amazon.com/images/I/412IXS1qNJL._SX329_BO1,204,203,200_.jpg"
+
+}
+
+
 function BookList() {
     return (
         <section className ='booklist'> 
-            <Book />
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+
+            <Book  img={secondBook.img} title={secondBook.title} author={secondBook.author} />
             
             
         </section>
     );
 }
-const author = 'Michelle Obama'
-const Book = () => {
-    const title = 'Becoming: A Guided Journal for Discovering Your Voice'
+
+const Book = (props) => {
+    console.log(props);
     return (
         <article className='book'>
-             <img src="https://m.media-amazon.com/images/I/81bFNmhKrTL._AC_UY218_.jpg" alt="Michelle Obama" />
-            <h1>{title}</h1>
-            <h4 >{author}</h4>
-
+             <img src={props.img} alt='' />
+            <h1>{props.title}</h1>
+            <h4 >{props.author}</h4>
+            
         </article>
     );
 };
