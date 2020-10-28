e743c5c95e8fff8baec062f9bc50c876cc24ce1e
tabit <tabithakavyu4@gmail.com>
2020-10-28 16:41:54 +0300

    Props destructuring

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 const firstBook = {
-    titlem :'Becoming: A Guided Journal for Discovering Your Voice',
+    title :'Becoming: A Guided Journal for Discovering Your Voice',
     author : 'Michelle Obama',
     img : "https://m.media-amazon.com/images/I/81bFNmhKrTL._AC_UY218_.jpg"
 
@@ -29,13 +29,13 @@ function BookList() {
     );
 }
 
-const Book = (props) => {
-    console.log(props);
+const Book = ({ img, title, author }) => {
+    //const { img, title, author } = props;
     return (
         <article className='book'>
-             <img src={props.img} alt='' />
-            <h1>{props.title}</h1>
-            <h4 >{props.author}</h4>
+             <img src={img} alt='' />
+            <h1>{title}</h1>
+            <h4 >{author}</h4>
             
         </article>
     );
