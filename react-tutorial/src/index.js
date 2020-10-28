3de62ffb2132e2002c68355bf888a0f0aebf9e02
tabit <tabithakavyu4@gmail.com>
2020-10-28 08:48:36 +0300

    JSX Rules

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+
+//JSX Rules
+//return single statement
+//div/ section / article or Fragment
+//use camelCase for html attribute
+//className instead of class
+//close every element
+//formatting: always use () after return
 function Greeting() {
-    return <h4>Hello World, Terrific Tuesday!!</h4>;
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
