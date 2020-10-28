fe2f009d6d9fc07b6effd8c87f2112648ef0773d
tabit <tabithakavyu4@gmail.com>
2020-10-28 09:07:33 +0300

    Nested components, React Tools

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -2,6 +2,34 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 
+// Nested components, React Tools
+function Greeting() {
+    return (
+        <div>
+            <Person />
+            <Message /> 
+        </div>
+    );
+}
+
+const Person = () => <h2>Chizaram Tabby</h2>;
+const Message = () => {
+    return (
+        <>
+        <p>All the things dem talk about me no be true.</p>
+        <p>Dem be saying all these things to get to you ooou.</p>
+        <p>Aii sisi no be true, no be true</p>
+        </> 
+    )     
+};
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
+
+
+
+
+
+
 //JSX Rules
 //return single statement
 //div/ section / article or Fragment
@@ -9,13 +37,3 @@ import ReactDom from 'react-dom'
 //className instead of class
 //close every element
 //formatting: always use () after return
-function Greeting() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
-}
-
-ReactDom.render(<Greeting />, document.getElementById('root'));
-
