88caaf77bc247d2f164501b2249a7a3203846ead
tabit <tabithakavyu4@gmail.com>
2020-10-29 19:28:27 +0300

    Events basics

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -31,7 +31,7 @@ function BookList() {
     
         <section className = 'booklist'>
                 {books.map((book) => {
-                const { img, title, author } = book;
+            
                     return <Book key={book.id}  {...book}></Book>
         })
     }
@@ -42,12 +42,24 @@ function BookList() {
 
 const Book = (props) => {
     const { img, title, author } = props;
+    const clickHandler = (e) => {
+        console.log(e.target);
+        console.log(e);
+        alert('hello world');
+    }
+    const complexExample = (author) => {
+        console.log(author);
+    }
+    
     return (
-        <article className='book'>
+        <article className='book' onMouseOver={() => {
+            console.log(title);
+        }}>
              <img src={img} alt='' />
             <h1>{title}</h1>
             <h4 >{author}</h4>
-            
+            <button type="button" onClick={clickHandler} > View Here</button>
+            <button type="button" onClick={() => complexExample(author)} > More Complex Examples</button>
             
             
         </article>
