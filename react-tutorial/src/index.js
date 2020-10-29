ba17ffa71209e7b50e51d18076a31192a93d9d30
tabit <tabithakavyu4@gmail.com>
2020-10-29 18:30:20 +0300

    Proper List

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -2,40 +2,51 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
 
-const firstBook = {
-    title :'Becoming: A Guided Journal for Discovering Your Voice',
-    author : 'Michelle Obama',
-    img : "https://m.media-amazon.com/images/I/81bFNmhKrTL._AC_UY218_.jpg"
+const books = [
+    {
+        title: 'Becoming: A Guided Journal for Discovering Your Voice',
+        author: 'Michelle Obama',
+        img: "https://m.media-amazon.com/images/I/81bFNmhKrTL._AC_UY218_.jpg"
 
-}
+    },
 
-const secondBook = {
-    title:'Conversations with RBG: Ruth Bader Ginsburg on Life, Love, Liberty, and Law',
-    author : 'Jeffery Rosen',
-    img : "https://images-na.ssl-images-amazon.com/images/I/412IXS1qNJL._SX329_BO1,204,203,200_.jpg"
+    {
+        title: 'Humans',
+        author: 'Brandon Stanton',
+        img: "https://images-na.ssl-images-amazon.com/images/I/51B-BOBtQtL._AC_SX184_.jpg"
 
-}
+    },
 
+    {
+        title: 'Greenlights',
+        author: 'Mathew McConaughey',
+        img: "https://images-na.ssl-images-amazon.com/images/I/41rbthdtokL._AC_SX184_.jpg"
+    },
+];
 
 function BookList() {
     return (
-        <section className ='booklist'> 
-            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
-
-            <Book  img={secondBook.img} title={secondBook.title} author={secondBook.author} />
-            
+    
+        <section className = 'booklist'>
+                {books.map((book) => {
+                const { img, title, author } = book;
+                return <Book book={book}></Book>
+        })
+    }
             
         </section>
     );
 }
 
-const Book = ({ img, title, author }) => {
-    //const { img, title, author } = props;
+const Book = (props) => {
+    const { img, title, author } = props.book;
     return (
         <article className='book'>
              <img src={img} alt='' />
             <h1>{title}</h1>
             <h4 >{author}</h4>
+            
+            
             
         </article>
     );
