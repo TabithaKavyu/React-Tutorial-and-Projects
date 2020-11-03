2a8a743775ed0c5b28d1af293bb2089fde894310
tabit <tabithakavyu4@gmail.com>
2020-11-03 09:41:59 +0300

    advanced react

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -6,8 +6,9 @@ import Book from './Book'
 
 function BookList() {
     return (
-    
-        <section className = 'booklist'>
+        <section className='booklist'>
+            
+
                 {books.map((book) => {
             
                     return <Book key={book.id}  {...book}></Book>
